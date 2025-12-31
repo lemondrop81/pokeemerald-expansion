5a8ccd1712c841e873ee9dbcba38f0f5ef376f64
Lemondrop81 <hacker814@hotmail.com>
2025-12-31 14:19:43 -0600

    Displaying the power of Frustration/Return in the Summary Screen

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -4159,12 +4159,23 @@ static void PrintMoveNameAndPP(u8 moveIndex)
 static void PrintMovePowerAndAccuracy(u16 moveIndex)
 {
     const u8 *text;
+    u8 monFriendship = GetMonData(&gPlayerParty[sMonSummaryScreen->curMonIndex], MON_DATA_FRIENDSHIP);
     if (moveIndex != MOVE_NONE)
     {
         FillWindowPixelRect(PSS_LABEL_WINDOW_MOVES_POWER_ACC, PIXEL_FILL(0), 53, 0, 19, 32);
 
         u32 power = GetMovePower(moveIndex);
-        if (power < 2)
+        if (moveIndex == MOVE_RETURN)
+       {
+           ConvertIntToDecimalStringN(gStringVar1, (10 * monFriendship / 25), STR_CONV_MODE_RIGHT_ALIGN, 3);
+           text = gStringVar1;
+       }
+       else if (moveIndex == MOVE_FRUSTRATION)
+       {
+           ConvertIntToDecimalStringN(gStringVar1, (10 * (MAX_FRIENDSHIP - monFriendship) / 25), STR_CONV_MODE_RIGHT_ALIGN, 3);
+           text = gStringVar1;
+       }
+       else if (power < 2)
         {
             text = gText_ThreeDashes;
         }
